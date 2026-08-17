13f7a6129cae1395ee0adad865a79db734d3def3
uzzolm835-ui <uzzolm835@gmail.com>
2026-08-17 09:52:00 +0600

    Create server.js

---
+++ server.js
@@ -0,0 +1,20 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({
+    success: true,
+    message: "Creator AI BD API is online!"
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
+});
